6bc9aa74417a9599756c38005323aa7579f423ad
JCamiloo <jcamilo.osorio15@gmail.com>
2019-12-15 15:27:40 -0500

    read post paginated

--- Backend/dist/routes/post.js
+++ Backend/dist/routes/post.js
@@ -12,6 +12,25 @@ const express_1 = require("express");
 const authentication_1 = require("../middlewares/authentication");
 const post_model_1 = require("./../models/post.model");
 const postRoutes = express_1.Router();
+//Get POST
+postRoutes.get('/', [authentication_1.checkToken], (req, res) => __awaiter(this, void 0, void 0, function* () {
+    const pagina = Number(req.query.pagina) || 1;
+    const skip = (pagina - 1) * 10;
+    const post = yield post_model_1.Post.find()
+        .sort({ _id: -1 })
+        .skip(skip)
+        .limit(10)
+        .populate('usuario', '-password')
+        .exec();
+    res.json({
+        success: true,
+        data: {
+            page: pagina,
+            posts: post
+        }
+    });
+}));
+//Create POST
 postRoutes.post('/', [authentication_1.checkToken], (req, res) => {
     const body = req.body;
     body.usuario = req.usuario._id;
--- Backend/routes/post.ts
+++ Backend/routes/post.ts
@@ -4,6 +4,28 @@ import { Post } from './../models/post.model';
 
 const postRoutes = Router();
 
+//Get POST
+postRoutes.get('/', [checkToken], async (req: any, res: Response) => {
+
+    const pagina = Number(req.query.pagina) || 1;
+    const skip = (pagina - 1) * 10;
+
+    const post = await Post.find()
+                           .sort({ _id: -1})
+                           .skip(skip)
+                           .limit(10)
+                           .populate('usuario', '-password')
+                           .exec();
+    res.json({
+        success: true,
+        data: {
+            page: pagina,
+            posts: post
+        }
+    });
+});
+
+//Create POST
 postRoutes.post('/', [checkToken], (req: any, res: Response) => {
     const body = req.body;
     body.usuario = req.usuario._id;
